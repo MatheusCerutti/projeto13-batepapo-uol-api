71126a9ffeaf9c830d22ec1b016cb554f632a3dc
Matheus <matheuscerutti96@gmail.com>
2023-01-16 17:11:51 -0300

    pegar o name e verificar se esta vazio

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
+import joi from 'joi';
 
 const server = express()
 server.use(express.json())
@@ -14,13 +15,38 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db("nomeDoBancoDeDados");
+    db = mongoClient.db("nomeDoBancoDeDados");
 });
 
 
-server.listen(5000, () => {
+server.listen(5001, () => {
     console.log('Servidor funfou')
 })
+
+try {
+    server.post("/participants", (req, res) => {
+
+        const infos = req.body
+
+        const validainfos = joi.object({
+            name: joi.string().required()
+        })
+
+        const validacao = validainfos.validate(infos.name, { abortEarly: true });
+
+        if (validacao.error) {
+            return res.status(422).send(validacao.error.details)
+        }
+
+    })
+
+    //vai vir o name, {name : joao}
+
+} catch (error) {
+    console.log(error)
+    res.status(500).send("Erro no servidor");
+}
+
 
 
 

213bdff809b4d82353cfe415906a87eb48e17846
Matheus <matheuscerutti96@gmail.com>
2023-01-17 13:46:48 -0300

    POST messages realizado

--- src/app.js
+++ src/app.js
@@ -82,6 +82,35 @@ server.get("/participants", async (req, res) => {
 })
 
 
+server.post("/messages", async (req, res) => {
+    const {to,text,type} = req.body
+    const {user} = req.headers
+
+
+    const validamessagem = joi.object({
+        from:joi.string().required(),
+        to:joi.string().required(),
+        text:joi.string().required(),
+        type:joi.string().required().valid("message","private_message")
+    })
+
+    const validacao = validamessagem.validate({ from: user,to,text,type });
+
+    if (validacao.error) {
+        return res.status(422).send(validacao)
+    }
+
+    const validarusuario = await db.collection("participants").findOne({name:user})
+
+    if (!validarusuario){
+        return res.status(422).send()
+    }
+
+    await db.collection("messages").insertOne({from:user,to,text,type,time:dayjs().format("HH:mm:ss")})
+
+    res.status(201).send("Deu certo")
+})
+
 
 
 
